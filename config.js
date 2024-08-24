dae0ca487cc32ddd1c0491a9cf531fbccf6fc0c7
AshenOne-xd <gamersaitamabuisness@gmail.com>
2024-08-24 15:06:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Kitty",
+    introTitle: 'Sweetheart Sannie',
+    introDesc: `Eh nigga idk what to cook
+    but i try my best to impress you
+    words arent enough to describe what i feel for you
+    so am KET KET KET KETING on my keyboard instead to impress you`,
+    btnIntro: '^^HiHi^^',
+    title: 'Aapne kaha tha apko kabhi saccha pyar hua hi nai abhi tak 🥰',
+    desc: 'Lekin wo sab badal gaya jab apko mai mila ',
+    btnYes: 'Correct bae <33',
+    btnNo: 'ABE JNLLLLLLLL',
+    question: 'I want to talk to you over call <3',
+    btnReply: 'I love u <33',
+    reply: 'Mujhe apko aur janna hai aur samjhna hai taaki mai apko aur beintehaan pyar kr saku <33333333',
+    mess: 'BRUHHHHHHHHH 🥰. Baeeeeeeeeeee 😘😘',
+    messDesc: 'Respond to this only on call aight?, I will be waiting....',
+    btnAccept: 'Okiiiii hun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://t.me/notzerotwooo?text=(your number here)
+}
